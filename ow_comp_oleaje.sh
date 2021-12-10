20a75725adcdc578baf897073320bcf0381341ca
Miguel Robles <miguel.robles@atmosfera.unam.mx>
2021-12-10 00:45:39 -0600

    algunas actualizaciones pequeñas en rutas. Se activó el fullscreen para videos de owgis

--- ow_comp_oleaje.sh
+++ ow_comp_oleaje.sh
@@ -1,6 +1,6 @@
-python3 owgis_rec_local.py http://pronosticos.unam.mx:8080/oleaje/ "Pacífico" "Altura" ~/owgis_videos/oleaje_PAC.mp4 "3.0,[-93.0,22]"
+python3 owgis_rec_local.py http://pronosticos.unam.mx:8080/oleaje/ "Pacífico" "Altura" ../owgis_videos/oleaje_PAC.mp4 "3.0,[-93.0,22]"
 #sleep 2
-python3 owgis_rec_local.py http://pronosticos.unam.mx:8080/oleaje/ "Golfo de México" "Altura" ~/owgis_videos/oleaje_GOM.mp4 "3.0,[-93,22]"
+python3 owgis_rec_local.py http://pronosticos.unam.mx:8080/oleaje/ "Golfo de México" "Altura" ../owgis_videos/oleaje_GOM.mp4 "3.0,[-93,22]"
 #sleep 2
-ffmpeg -y -i ~/owgis_videos/oleaje_GOM.mp4 -i ~/owgis_videos/oleaje_PAC.mp4 -filter_complex "blend=all_expr='if(gt(X,Y*1.3),A,B)'" -pix_fmt yuv420p ~/owgis_videos/oleaje_comp.mp4 
+ffmpeg -y -i ../owgis_videos/oleaje_GOM.mp4 -i ../owgis_videos/oleaje_PAC.mp4 -filter_complex "blend=all_expr='if(gt(X,Y*1.3),A,B)'" -pix_fmt yuv420p ../owgis_videos/oleaje_comp.mp4 
 
--- owgis_all.sh
+++ owgis_all.sh
@@ -2,9 +2,9 @@
 #sleep 2
 #python3 owgis_rec_local.py http://pronosticos.unam.mx:8080/global GFS Temperature ~/owgis_videos/globalT.mp4 "0.35,[0,0]"
 #sleep 2
-python3 owgis_rec_local.py http://pronosticos.unam.mx:8080/meteorologico "República Mexicana" Viento ~/owgis_videos/globalW.mp4 "3.0,[-93.0,22]"
+python3 owgis_rec_local.py http://pronosticos.unam.mx:8080/meteorologico "República Mexicana" Viento ../owgis_videos/globalW.mp4 "3.0,[-93.0,22]"
 sleep 2
-python3 owgis_rec_local.py http://pronosticos.unam.mx:8080/meteorologico "República Mexicana" "Temperatura a 2 metros" ~/owgis_videos/globalT.mp4 "3.0,[-93.0,22]"
+python3 owgis_rec_local.py http://pronosticos.unam.mx:8080/meteorologico "República Mexicana" "Temperatura a 2 metros" ../owgis_videos/globalT.mp4 "3.0,[-93.0,22]"
 sleep 2
 ./ow_comp_oleaje.sh
 ./ow_comp_marea.sh
