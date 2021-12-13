f671ac526df3ea8f46c14d3c2fff1d29ab7447ce
Miguel Robles <miguel.robles@atmosfera.unam.mx>
2021-12-13 08:27:23 -0600

    tiempo de espera aumentado

--- owgis_rec_local.py
+++ owgis_rec_local.py
@@ -138,7 +138,7 @@ def get_percent(perc_str):
         
 time.sleep(5)
 print('load:',get_percent(load.text))
-for i in range(90):
+for i in range(150):
     time.sleep(2)
     pc_val=get_percent(load.text)
     print('Iter:',i,end=' ')
